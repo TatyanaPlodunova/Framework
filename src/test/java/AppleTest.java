938b71262f10fee0bf4a9a6ce945c142615e5698
t.plodunova <t.plodunova@21vek.by>
2024-02-06 17:59:42 +0300

    second commit

--- src/test/java/AppleTest.java
+++ src/test/java/AppleTest.java
@@ -1,11 +1,17 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class AppleTest {
     public static void main(String[] args) {
+        // 1. Открыть сайт
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.apple.com/");
@@ -15,15 +21,19 @@ public class AppleTest {
         searchButton.click();
 
         // 3. Найти "iPhone 15"
-        WebElement searchInput = driver.findElement(By.xpath("//input[@class='globalnav-searchfield-input']"));
+        int seconds = 5;
+        Duration duration = Duration.ofSeconds(seconds);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+        WebElement searchInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class='globalnav-searchfield-input']")));
         searchInput.isDisplayed();
-        System.out.println(searchInput.isDisplayed());
-        // проверить видимость черной выпадашки - по элементу который там есть (например кусочек текста)
-        // проверить видимость поля с дополнительным аттрибутом(на скрине)
+        searchInput.sendKeys("iphone 15");
+        searchInput.sendKeys(Keys.ENTER);
 
         // 4. Проверить, что результаты поиска содержат "iPhone 15"
-        // локатор который не затрагивает новости, а затрагивает только телефоны
-
+        WebElement searchResults = driver.findElement(By.xpath("//div[@class='rf-serp-explore-curated']"));
+        String searchResultsText = searchResults.getText();
+        searchResultsText.contains("iPhone 15");
+        System.out.println(searchResultsText.contains("iPhone 15"));
         driver.quit();
     }
 }
